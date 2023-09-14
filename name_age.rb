8fe158897968197698b951bdd35b843370ba4297
marcosvieira80 <marcosjosevieira@hotmail.com>
2023-09-14 12:31:01 -0300

    Exercícios ruby vscode

---
+++ name_age.rb
@@ -0,0 +1,9 @@
+print "Digite um nome: "
+
+nome = gets.chomp
+
+print "Digite uma idade: "
+
+idade = gets.chomp.to_i
+
+puts "#{nome} tem #{idade} anos"
---
+++ operacao.rb
@@ -0,0 +1,12 @@
+print "Digite um número: "
+
+n1 = gets.chomp.to_i
+
+print "Digite outro número: "
+
+n2 = gets.chomp.to_i
+
+puts "SOMA = #{n1 + n2}"
+puts "SUBTRAÇÃO = #{n1 - n2}"
+puts "MULTIPLICAÇÃO = #{n1 * n2}"
+puts "DIVISÃO = #{n1 / n2}"
